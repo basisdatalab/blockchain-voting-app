4d208e4731bb91ead8379bdfc3f0e73881c7d202
Agus Janardana <bjanardana@gmail.com>
2021-01-29 01:52:47 +0800

    ADDED ENV

--- app/helpers/passport-configuration.js
+++ app/helpers/passport-configuration.js
@@ -25,43 +25,43 @@ passport.use(new GoogleStrategy({
     }
 ));
 
-passport.use(new GoogleStrategy({
-    clientID: "665311001771-tlkhbgkgcb0uvaqgsael87ati33sck0l.apps.googleusercontent.com",
-    clientSecret: "g-Ja7yVVU4LJituJUTlVsd-3",
-    callbackURL: "http://localhost:3007/stakeholder/get/callback"
-},
-    function (accessToken, refreshToken, profile, done) {
-        console.log(profile)
-        // User.findOrCreate({ googleId: profile.id }, function (err, user) { untuk melihat di db ini
-        return done(null, profile);
-        // });
-    }
-));
+// passport.use(new GoogleStrategy({
+//     clientID: "665311001771-tlkhbgkgcb0uvaqgsael87ati33sck0l.apps.googleusercontent.com",
+//     clientSecret: "g-Ja7yVVU4LJituJUTlVsd-3",
+//     callbackURL: "http://localhost:3007/stakeholder/get/callback"
+// },
+//     function (accessToken, refreshToken, profile, done) {
+//         console.log(profile)
+//         // User.findOrCreate({ googleId: profile.id }, function (err, user) { untuk melihat di db ini
+//         return done(null, profile);
+//         // });
+//     }
+// ));
 
 
-passport.use(new GoogleStrategy({
-    clientID: "665311001771-tlkhbgkgcb0uvaqgsael87ati33sck0l.apps.googleusercontent.com",
-    clientSecret: "g-Ja7yVVU4LJituJUTlVsd-3",
-    callbackURL: "http://localhost:3007/stakeholder/get/callback"
-},
-    function (accessToken, refreshToken, profile, done) {
-        console.log(profile)
-        // User.findOrCreate({ googleId: profile.id }, function (err, user) { untuk melihat di db ini
-        return done(null, profile);
-        // });
-    }
-));
+// passport.use(new GoogleStrategy({
+//     clientID: "665311001771-tlkhbgkgcb0uvaqgsael87ati33sck0l.apps.googleusercontent.com",
+//     clientSecret: "g-Ja7yVVU4LJituJUTlVsd-3",
+//     callbackURL: "http://localhost:3007/stakeholder/get/callback"
+// },
+//     function (accessToken, refreshToken, profile, done) {
+//         console.log(profile)
+//         // User.findOrCreate({ googleId: profile.id }, function (err, user) { untuk melihat di db ini
+//         return done(null, profile);
+//         // });
+//     }
+// ));
 
-passport.use(new GoogleStrategy({
-    clientID: "951427217505-jluqul4vr760eh8v0kur16260rft7u0u.apps.googleusercontent.com",
-    clientSecret: "86jl8_3SqlFTItJUDDUWb5pn",
-    callbackURL: "http://localhost:3007/api/v1/voting/voter/login/callback"
-},
-    function (accessToken, refreshToken, profile, done) {
+// passport.use(new GoogleStrategy({
+//     clientID: "951427217505-jluqul4vr760eh8v0kur16260rft7u0u.apps.googleusercontent.com",
+//     clientSecret: "86jl8_3SqlFTItJUDDUWb5pn",
+//     callbackURL: "http://localhost:3007/api/v1/voting/voter/login/callback"
+// },
+//     function (accessToken, refreshToken, profile, done) {
 
-        // User.findOrCreate({ googleId: profile.id }, function (err, user) { untuk melihat di db ini
-        return done(null, profile);
-        // });
-    }
-));
+//         // User.findOrCreate({ googleId: profile.id }, function (err, user) { untuk melihat di db ini
+//         return done(null, profile);
+//         // });
+//     }
+// ));
 
