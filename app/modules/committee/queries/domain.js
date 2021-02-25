5282afae366e6d9384d269e0d8a9f11484567ec9
dimasguintana <dguint@outlook.com>
2021-02-25 13:48:44 +0700

    login register commmittee udah bisa

--- app/modules/committee/queries/domain.js
+++ app/modules/committee/queries/domain.js
@@ -1,12 +1,20 @@
 //Karena ini adalah domain yang berada pada repositories query, maka isinya hanya bisa get data
-
+const { pool } = require('../../../helpers/dbConfig')
+require('pg')
 class Login {
-    getLogin(payload){
-        const displayName = payload.name
-        const userEmail = payload.email
-        const profilePicture = payload.picture
-        const login = `Selamat datang ` + displayName + " dengan email: " + userEmail + ' dan link foto: ' + profilePicture
-        return login
+    async getLogin(payload) {
+        const email = payload.email
+        const user = await pool.query("SELECT * FROM committee WHERE email = $1", [email])
+        if(user.rows.length === 0){
+            return {
+                status: 'false',
+                name : 'Tidak terdaftar sebagai committee'
+            }
+        }
+        return {
+            status: 'true',
+            name : 'Selamat datang committee!'
+        }
     }
     notLogin(){
         const notLogin = `anda belum login`
@@ -22,4 +30,27 @@ class Login {
     }
 }
 
-module.exports = Login
+class Register {
+    async getRegister(payload) {
+        const email = payload.email
+        const user = await pool.query("SELECT * FROM committee WHERE email = $1", [email])
+        if(user.rows.length === 0){
+            var sql = `INSERT INTO committee VALUES ($1, $2, $3, $4)`;
+            var params = [payload.id, payload.name, payload.sid, payload.email]
+            pool.query(sql, params)
+            return {
+                status: 'True',
+                name : 'Anda berhasil mendaftar sebagai committee.'
+            }
+        }
+        return {
+            status: 'false',
+            name : 'Anda sudah terdaftar sebagai committee.'
+        }
+    }
+}
+
+module.exports = {
+    Login: Login,
+    Register: Register
+}
--- app/modules/committee/queries/query_handler.js
+++ app/modules/committee/queries/query_handler.js
@@ -3,28 +3,35 @@ const getCommittee = require('./domain')
 // fungsi-fungsi disini akan inisialisasi object module
 
 const getLogin = (payload) => {
-    const getLoginCommittee = new getCommittee()
+    const getLoginCommittee = new getCommittee.Login()
     return getLoginCommittee.getLogin(payload)
 }
 
 const notLogin = () => {
-    const notLoginCommittee = new getCommittee()
+    const notLoginCommittee = new getCommittee.Login()
     return notLoginCommittee.notLogin()
 }
 
 const notVerif = () => {
-    const notVerifCommittee = new getCommittee()
+    const notVerifCommittee = new getCommittee.Login()
     return notVerifCommittee.notVerif()
 }
 
 const failLogin = () => {
-    const failLoginCommittee = new getCommittee()
+    const failLoginCommittee = new getCommittee.Login()
     return failLoginCommittee.failLogin()
+}
+
+const getRegister = (payload) => {
+    const getRegisterCommittee = new getCommittee.Register()
+    // console.log(getRegisterCommittee)
+    return getRegisterCommittee.getRegister(payload)
 }
 
 module.exports = {
     getLogin,
     notLogin,
     notVerif,
-    failLogin
+    failLogin,
+    getRegister
 }
